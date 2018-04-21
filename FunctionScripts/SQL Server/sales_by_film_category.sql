c65a62db4e148e1223d5a59371f0aabe7b6be7ce
moscas-poscas@yandex.ru <@WSXzaq1>
2018-04-21 12:57:09 -0400

    reworks

--- FunctionScripts/SQL Server/sales_by_film_category.sql
+++ FunctionScripts/SQL Server/sales_by_film_category.sql
@@ -4,10 +4,10 @@ SELECT
 c.name AS category
 , SUM(p.amount) AS total_sales
 FROM payment AS p
-INNER JOIN rental AS r ON p.rental_id = r.rental_id
-INNER JOIN inventory AS i ON r.inventory_id = i.inventory_id
-INNER JOIN film AS f ON i.film_id = f.film_id
-INNER JOIN film_category AS fc ON f.film_id = fc.film_id
-INNER JOIN category AS c ON fc.category_id = c.category_id
+  INNER JOIN rental AS r ON p.rental_id = r.rental_id
+  INNER JOIN inventory AS i ON r.inventory_id = i.inventory_id
+  INNER JOIN film AS f ON i.film_id = f.film_id
+  INNER JOIN film_category AS fc ON f.film_id = fc.film_id
+  INNER JOIN category AS c ON fc.category_id = c.category_id
 GROUP BY c.name
 
